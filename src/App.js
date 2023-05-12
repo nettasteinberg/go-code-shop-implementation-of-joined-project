52be79abed65fdfd8dbec6d3f70ac1c5369f91dc
Netta Steinberg <netta.steinberg@gmail.com>
2023-05-12 17:44:59 +0300

    Added 'add to cart' button, fixed the way the buttons work in each product box

--- src/App.js
+++ src/App.js
@@ -11,8 +11,29 @@ function App() {
   const [sortByValue, setSortByValue] = useState("None");
   const [itemsInCart, setItemsInCart] = useState({});
 
+  const incrementProduct = (setFunc) => {
+    setFunc((prev) =>  prev + 1);
+  };
+  
+  const decrementProduct = (setFunc) => {
+    setFunc((prev) => (prev === 0 ? prev : prev - 1));
+  };
+
+  const addToCart = (productName, amount, price, setFunc) => {
+    if (amount === 0) {
+      return;
+    }
+    if (productName in itemsInCart) {
+      itemsInCart[productName][0] += amount;
+    } else {
+      itemsInCart[productName] = [amount, price];
+    }
+    setItemsInCart({...itemsInCart});
+    setFunc(0);
+  }
+
   return (
-    <MyContext.Provider value={{categories, setCategories, filterByValue, setFilterByValue, setSortByValue, itemsInCart, setItemsInCart}}>
+    <MyContext.Provider value={{categories, setCategories, filterByValue, setFilterByValue, setSortByValue, itemsInCart, setItemsInCart, incrementProduct, decrementProduct, addToCart}}>
       <div className="App">
         <Nav/>
         <Cart/>
--- src/components/AddSubtractProduct/AddSubtractProduct.js
+++ src/components/AddSubtractProduct/AddSubtractProduct.js
@@ -1,40 +1,43 @@
-import { useContext } from "react";
-import { MyContext } from "../../MyContext";
+// import { useContext } from "react";
+// import { MyContext } from "../../MyContext";
+// import { Button } from "../Button/Button";
 
-export const AddSubtractProduct = ({title, price}) => {
+// export const AddSubtractProduct = ({title, price}) => {
 
-    const { itemsInCart, setItemsInCart } = useContext(MyContext);
+//     const { itemsInCart, setItemsInCart } = useContext(MyContext);
 
-    const subtract = () => {
-        if (title in itemsInCart) {
-          if (itemsInCart[title][0] > 1) {
-            itemsInCart[title][0] = itemsInCart[title][0] - 1;
-            setItemsInCart({...itemsInCart});
-          } else {
-            delete itemsInCart[title];
-            setItemsInCart({...itemsInCart}); 
-          }
-        }
-        console.log(itemsInCart);
-      }
+//     const subtract = () => {
+//         if (title in itemsInCart) {
+//           if (itemsInCart[title][0] > 1) {
+//             itemsInCart[title][0] = itemsInCart[title][0] - 1;
+//             setItemsInCart({...itemsInCart});
+//           } else {
+//             delete itemsInCart[title];
+//             setItemsInCart({...itemsInCart}); 
+//           }
+//         }
+//         console.log(itemsInCart);
+//       }
     
-      const add = () => {
-        if (title in itemsInCart) {
-          itemsInCart[title][0] = itemsInCart[title][0] + 1;
-          setItemsInCart({...itemsInCart});
-        } else {
-          itemsInCart[title] = [1, price];
-          console.log(itemsInCart);
-          setItemsInCart({...itemsInCart});
-        }
-        console.log(itemsInCart);
-      }
+//     const add = () => {
+//       if (title in itemsInCart) {
+//         itemsInCart[title][0] = itemsInCart[title][0] + 1;
+//         setItemsInCart({...itemsInCart});
+//       } else {
+//         itemsInCart[title] = [1, price];
+//         console.log(itemsInCart);
+//         setItemsInCart({...itemsInCart});
+//       }
+//       console.log(itemsInCart);
+//     }
     
-    return (
-    <div className="addSubtractProduct">
-        <button onClick={subtract}>-</button>
-        <p>{(title in itemsInCart ? itemsInCart[title][0] : 0)}</p>
-        <button onClick={add}>+</button>
-    </div>
-    )
-}
+//     return (
+//     <div className="addSubtractProduct">
+//         {/* <button onClick={subtract}>-</button> */}
+//         <Button onClick={subtract} text="-"/>
+//         <p>{(title in itemsInCart ? itemsInCart[title][0] : 0)}</p>
+//         {/* <button onClick={add}>+</button> */}
+//         <Button onClick={add} text="+"/>
+//     </div>
+//     )
+// }
