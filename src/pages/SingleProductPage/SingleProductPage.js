69ef42089a001092823a416c4409c9873f0c2f56
Netta Steinberg <netta.steinberg@gmail.com>
2023-05-13 16:22:06 +0300

    Single product page works using by finding the specific product in the array of products, without fetching the specific product from the API. I left the code for the fetch just in case.

--- src/pages/SingleProductPage/SingleProductPage.js
+++ src/pages/SingleProductPage/SingleProductPage.js
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import "./SingleProductPage.css";
+import { MyContext } from '../../MyContext';
 
-export const SingleProductPage = () => {
+const SingleProductPage = () => {
   const params = useParams();
-  const [singleProduct, setSingleProduct] = useState({});
-  const [currentId, setCurrentId] = useState(-1);
+  // const [singleProduct, setSingleProduct] = useState({});
+  // const [currentId, setCurrentId] = useState(-1);
 
-  useEffect(() => {
-    fetchSingleProduct(params.id);
-  }, [])
+  // useEffect(() => {
+  //   fetchSingleProduct(params.id);
+  // }, [])
 
-  const fetchSingleProduct = async (id) => {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`)
-    const data = await response.json();
-    console.log(data.id, data.price);
-    setSingleProduct(data);
-    setCurrentId(data.id);
-  }
+  // const fetchSingleProduct = async (id) => {
+  //   const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+  //   const data = await response.json();
+  //   console.log(data.id, data.price);
+  //   setSingleProduct(data);
+  //   setCurrentId(data.id);
+  // }
+
+  const {products} = useContext(MyContext);
+  const singleProduct = products.find(p => {console.log(p.id, p.id === parseInt(params.id)); return p.id === parseInt(params.id)});
 
   return (
     <div>
@@ -28,7 +32,7 @@ export const SingleProductPage = () => {
         </div>
         <div className="product-info">
           <h5>{singleProduct.title}</h5>
-          {singleProduct.id === currentId  && <h6>{singleProduct.price}$</h6>}
+          {/*singleProduct.id === currentId  && */<h6>{singleProduct.price}$</h6>}
         </div>
       </div>
     </div>
