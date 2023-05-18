5f67b44a6d597a8232abe64f6d933f2c93ab258e
Netta Steinberg <netta.steinberg@gmail.com>
2023-05-18 15:43:17 +0300

    Replaced the content on Product with code related to MUI Card

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,29 +1,47 @@
+import { CardActions, CardMedia } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import React, { useContext, useState } from 'react';
-import { Button } from '../Button/Button'; 
+import { useNavigate } from 'react-router-dom';
 import { MyContext } from '../../MyContext';
-import { useNavigate } from "react-router-dom";
-import './Product.css'
+import { Button } from '../Button/Button';
+import "./Product.css";
 
-export const Product = ({src, title, price, id}) => { 
-  const navigate = useNavigate();
-  const {incrementProduct, decrementProduct, addToCart} = useContext(MyContext);
+export const Product = ({ src, title, price, id }) => {
+  const { incrementProduct, decrementProduct, addToCart } = useContext(MyContext);
   const [count, setCount] = useState(0);
+  const navigate = useNavigate();
 
   return (
-    <div className="product-card">
-      <div className="product-image" onClick={() => navigate(`product/${id}`)}>
-        <img src={src} alt={title}/>
-      </div>
-      <div className="product-info">
-        <h5>{title}</h5>
-        <h6>{price}$</h6>
-      </div>
-      <div className="addToCartContainer">
-          <Button onClick={() => decrementProduct(setCount)} text={"-"} />
-          <p>{count}</p>
-          <Button onClick={() => incrementProduct(setCount)} text={"+"} />
-      </div>
-      <Button onClick={() => addToCart(title, count, price, setCount)} text={"ADD TO CART"} />
-    </div>
+    <Card sx={{ maxWidth: 200 }} className="product-card">
+      <CardMedia
+        component="img"
+        alt={title}
+        height="200"
+        // width="240"
+        image={src}
+        gap="5"
+        onClick={() => navigate(`product/${id}`)}
+      />
+      <CardContent height="200" className="product-info">
+        <Typography gutterBottom variant="h6" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {price}$
+        </Typography>
+      </CardContent>
+      <CardActions className="addToCartContainer">
+        <Button onClick={() => decrementProduct(setCount)} text={"-"} />
+        <p>{count}</p>
+        <Button onClick={() => incrementProduct(setCount)} text={"+"} />
+      </CardActions>
+      <CardActions>
+        <Button onClick={() => addToCart(title, count, price, setCount)} text={"ADD TO CART"} />
+      </CardActions>
+    </Card>
   );
-};
+}
+
+export default Product
