5ed9dce765863f23b036e759d01f091c733a425f
Netta Steinberg <netta.steinberg@gmail.com>
2023-06-13 14:34:15 +0300

    Changed the port to be the same as in the server

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-export const GET_ALL_PRODUCTS_PATH = process.env.NODE_ENV === "development" ? "http://localhost:8000/" : "the url for when the server is in the cloud";
-export const GET_PRODUCT_BY_ID = process.env.NODE_ENV === "development" ? "http://localhost:8000/" + "product/" : "the url for when the server is in the cloud";
+export const GET_ALL_PRODUCTS_PATH = process.env.NODE_ENV === "development" ? "http://localhost:8001/api/" : "the url for when the server is in the cloud";
+export const GET_PRODUCT_BY_ID = process.env.NODE_ENV === "development" ? "http://localhost:8001/api/" + "product/" : "the url for when the server is in the cloud";
